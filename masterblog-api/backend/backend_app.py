bcbbfad0a808bdfad67c6eef124d326b908c9087
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-28 11:43:45 +0200

    Implement add endpoint for creating new blog posts with validation and error handling

--- masterblog-api/backend/backend_app.py
+++ masterblog-api/backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,42 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def validate_post_data(data):
+    if 'title' not in data or 'content' not in data:
+        return False
+    return True
+
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'POST':
+        new_post =  request.get_json()
+        if not validate_post_data(new_post):
+            return jsonify({'error': 'Invalid post data'}), 400
+        #gerenrate new id
+        new_id = max(post["id"] for post in POSTS) + 1
+        new_post["id"] = new_id
+
+        POSTS.append(new_post)
+
+        return jsonify(new_post), 201
+
+    else:
+        return jsonify(POSTS)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Not Found"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({"error": "Method Not Allowed"}), 405
+
+
+
 
 
 if __name__ == '__main__':
